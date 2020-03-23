9c4a1eb5b25f86381880a7b775fca8cacd678ea6
Dennis Rivera <rivd19@outlook.com>
2020-03-23 05:33:56 -0400

    Built out the starting model

--- lib/app.py
+++ lib/app.py
@@ -1,9 +1,29 @@
 from peewee import *
+from datetime import date
 
 db = PostgresqlDatabase('contactbook', user='DRivera', password='',
                         host='localhost', port=5432)
 
+
+
+class BasesModel(Model): 
+  class Meta: 
+    database = db
+
+class Contact(BasesModel):
+  entrydate = DateField()
+  name = CharField()
+
+
 db.connect()
+db.drop_tables([Contact])
+db.create_tables([Contact])
+
+contact = Contact(entrydate=date(2020, 3, 23), name="Dennis")
+contact.save()
+
+
+
 
 
 
